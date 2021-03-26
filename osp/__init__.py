7a859f0c295372c4e74aa76022d4713de6097e6f
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-03-26 18:48:46 +0530

    Directory restructuring

--- osp/__init__.py
+++ osp/__init__.py
@@ -4,7 +4,7 @@ from mongoengine import *
 connect(host='mongodb+srv://ospapp:tyaLmQbvP6rJU4uY@smmh.yappb.mongodb.net/ospDatabase?retryWrites=true&w=majority')
 
 # make all relevant classes available directly
-# from .testclass import TestClass, TestClass2, TestClass3
+from .classes import *
 
-from .user import Manager, Buyer, Seller
-from .address import Address
+# make all relevant interfaces available directly
+from .interfaces import *
---
+++ osp/classes/__init__.py
@@ -0,0 +1,5 @@
+from .testclass import TestClass, TestClass2, TestClass3
+
+# make all relevant classes available directly
+from .user import Manager, Buyer, Seller
+from .address import Address
