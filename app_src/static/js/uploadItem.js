3f4e24a589652bca188006ae93b23d38fc5afcc4
JhadotAnimesh <jha.animesh01@gmail.com>
2021-04-02 23:39:49 +0530

    half upload

--- app_src/static/js/uploadItem.js
+++ app_src/static/js/uploadItem.js
@@ -4,27 +4,35 @@ const Upload = {
     delimiters: ['[[', ']]'],
     data() {
         return {
-            counter:0,
-            categories:[]
+            counter: 0,
+            categories: []
         }
     },
-    methods: {
-        async getCats(){
-            const requestOptions = {
-                method: "POST",
-                credentials: "include",
-                body: JSON.stringify(''),
-                cache: "no-cache",
-                headers: new Headers({
-                    "content-type": "application/json"
-                })
-            }
-            const response = await fetch(`${window.origin}/api/category_list`, requestOptions);
-            const data = await response.json();
-            this.categories=data.list_cat;
-
-
+    created(){
+        const requestOptions = {
+            method: "POST",
+            credentials: "include",
+            body: JSON.stringify(''),
+            cache: "no-cache",
+            headers: new Headers({
+                "content-type": "application/json"
+            })
         }
+        // const response = await fetch(`${window.origin}/api/category_list`, requestOptions);
+        // const data = await response.json();
+        // this.categories=data.list_cat;
+        fetch(`${window.origin}/api/category_list`, requestOptions)
+            .then(function (response) {
+                response.json()
+                    .then(function (data) {
+                        this.categories = data.list_cat;
+                    })
+            })
+            .catch(function (error) { console.log(error) });
+        console.log("hi");
+    },
+    methods: {
+
     }
 }
 
