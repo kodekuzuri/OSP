4506da05ecd1c05021b4407f34238dcfa204adac
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-04-02 16:21:00 +0530

    debugging

--- osp/__init__.py
+++ osp/__init__.py
@@ -1,7 +1,9 @@
 import mongoengine as me
-import os
+import os, sys
 
 # connect to database
+print(os.environ["OSP_DATABASE"])
+sys.stdout.flush()
 me.connect(host=os.environ["OSP_DATABASE"]) # make this environment variable finally
 
 # make all relevant classes available directly
