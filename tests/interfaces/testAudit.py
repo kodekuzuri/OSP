fdb3c9cc8a2c839491e6f4cc06431e06f021c5a7
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-04-06 00:11:59 +0530

    test audit done

--- tests/interfaces/testAudit.py
+++ tests/interfaces/testAudit.py
@@ -1,19 +1,25 @@
 import sys
 sys.path.append(".")
 
-from osp import Buyer, Item, Seller
+from osp import Buyer, Item, Seller, Category, BuyRequest
+
+Category.AddCategory("test_cat_1")
+Category.AddCategory("test_cat_2")
+
+for req in BuyRequest.objects():
+    req.delete()
 
 print("Testing audit.")
 print("creating dummy input for testing")
 try:
-    buyer_test1=Buyer(city="test one",password="abcd",email="testemail@email.com",name="test_buyer",number="9717911976")
+    buyer_test1=Buyer(city="test one",password="abcd",email="testemail@email.com",name="test_buyer1",number="9717911976")
     buyer_test1.save()
-    buyer_test1.uniqueid=str(buyer_test.id)
+    buyer_test1.uniqueid=str(buyer_test1.id)
     buyer_test1.save()
 
-    buyer_test2=Buyer(city="test one",password="abcd",email="testemail@email.com",name="test_buyer",number="9717911976")
+    buyer_test2=Buyer(city="test one",password="abcd",email="testemail@email.com",name="test_buyer2",number="9717911976")
     buyer_test2.save()
-    buyer_test2.uniqueid=str(buyer_test.id)
+    buyer_test2.uniqueid=str(buyer_test2.id)
     buyer_test2.save()
 
     seller_test=Seller(city="test",password="abcd1",email="testemail1@email.com",name="test_seller",number="9717911976")
@@ -25,7 +31,7 @@ try:
     with open("./tests/classes/jester_pc.jpeg", "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     i1=Item()
-    i1.createItem(name="test_buyrequest_item_heavy",category="test_cat_1",seller=seller_test.name,price=1000,company="test",city="test",photo=encoded_string,info="nice heavy laptop",age=2,weight=501)
+    i1.createItem(name="test_buyrequest_item_heavy",category="test_cat_1",seller=seller_test.name,price=1000,company="test",city="test",photo=encoded_string,info="nice heavy laptop",age=2,weight=5)
     i1.uploadToDB()
     
     i2=Item()
@@ -46,17 +52,28 @@ except Exception as e:
 
 
 print("#### checking category deletions effect on items")
-Category.RemoveCategory(Category.objects(name="test_cat_1").uniqueid)
+# Category.RemoveCategory(Category.objects(name="test_cat_1").first().uniqueid)
 print(i1.category)
 print(i3.category)
+
+print("#### print existing buyreqs")
+for req in BuyRequest.objects():
+    print(req.item.name, req.buyer.name)
+
+print("#### checking buy requests after item deletion")
+i1.delete()
+for req in BuyRequest.objects():
+    print(req.item.name, req.buyer.name)
 
 
 print("deleting dummies")
 try:
-    buyer_test.delete()
+    buyer_test1.delete()
+    buyer_test2.delete()
     seller_test.delete()
-    i1.delete()
     i2.delete()
+    i3.delete()
+    Category.RemoveCategory(Category.objects(name="test_cat_2").first().uniqueid)
     print("success")
 except:
     print("failed")
