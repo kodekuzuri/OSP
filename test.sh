4e3f629846474054e325b53968e92942b9c27c15
JhadotAnimesh <jha.animesh01@gmail.com>
2021-04-05 23:29:31 +0530

    golden generation

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 python tests/interfaces/testLogin.py >> test_goldens/login.txt   
 python tests/classes/testBuyRequests.py >> test_goldens/BuyRequests.txt  
 python tests/classes/testSeller.py >> test_goldens/Seller.txt
+python tests/classes/testBuyer.py >> test_goldens/Buyer.txt
 python tests/classes/testBuyer.py >> test_goldens/Buyer.txt  
+
